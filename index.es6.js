5cb4e90eb2956e3bb5c3593e9b20ef76593843c6
Eugene Chechurin <e.chechurin@gmail.com>
2014-11-27 15:42:01 +0200

    named function instead of annonymous

--- index.es6.js
+++ index.es6.js
@@ -1,7 +1,7 @@
 var encode = encodeURIComponent
 var decode = decodeURIComponent
-
-export default function (name, value, options) {
+  
+export default function cookie(name, value, options) {
   if (arguments.length < 2) return get(name)
   set(name, value, options)
 }
