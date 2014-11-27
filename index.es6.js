b34332192d39de33aa15bdcc5030fcb0adda92bd
Eugene Chechurin <e.chechurin@gmail.com>
2014-11-27 12:50:06 +0200

    initial commit

---
+++ index.es6.js
@@ -0,0 +1,41 @@
+var encode = encodeURIComponent
+var decode = decodeURIComponent
+
+export default function (name, value, options) {
+  if (arguments.length < 2) return get(name)
+  set(name, value, options)
+}
+
+function set(name, value, options = {}) {
+  var str = `${encode(name)}=${encode(value)}`
+
+  if (value == null) options.maxage = -1
+
+  if (options.maxage) {
+    options.expires = new Date(+new Date() + options.maxage)
+  }
+
+  if (options.path) str += '; path=' + options.path
+  if (options.domain) str += '; domain=' + options.domain
+  if (options.expires) str += '; expires=' + options.expires.toUTCString()
+  if (options.secure) str += '; secure'
+
+  document.cookie = str
+}
+
+function get(name) {
+  var cookies = parse(document.cookie)
+  return !!name ? cookies : cookies[name]
+}
+
+function parse(str) {
+  var obj = {},
+    pairs = str.split(/ *; */)
+  
+  if (!!pairs[0]) return obj
+  
+  for (let pair of pairs) {
+    pair = pair.split('=')
+    obj[decode(pair[0])] = decode(pair[1])
+  }
+}
